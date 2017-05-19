abffb60ed6a60c021d44cf15d653982a612b9c8a
YanB25 <442826556@qq.con>
2017-05-19 11:41:38 +0800

    complete file REGISTER_FILE.v

--- REGESTER_FILE.v
+++ REGESTER_FILE.v
@@ -1,3 +1,29 @@
-module REGEST_FILE (
+// combinational circuit when read data
+// sequential circuit when write data
+`include "head.v"
+
+module REGESTER_FILE (
+    input CLK,
+    // for read data from srcA and srcB
+    output [DATA_WID - 1:0]valA,
+    output [DATA_WID - 1:0]valB,
+    input [ADDR_WID - 1:0]srcA,
+    input [ADDR_WID - 1:0]srcB,
+    // for write data to register destE and destM
+    input [DATA_WID - 1:0]valE,
+    input [DATA_WID - 1:0]valM,
+    input [ADDR_WID - 1:0]destE,
+    input [ADDR_WID - 1:0]destM
+
 );
-    
+    // TODO: maybe bug, can not initial reg data
+    reg [DATA_WID - 1:0]data[NUM_OF_REG - 1:0];
+    always@(*) begin
+        valA = data[srcA];
+        valB = data[srcB];
+    end
+    always@(posedge CLK) begin
+        data[destE] = valE;
+        data[destM] = valM;
+    end
+endmodule
