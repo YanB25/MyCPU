dca1e49a979e80ee72426b9b81f27627be80fb60
YanB25 <442826556@qq.con>
2017-05-19 12:07:57 +0800

    complete testbench for REG_FILE

--- REGESTER_FILE_tb.v
+++ REGESTER_FILE_tb.v
@@ -3,7 +3,47 @@
 `include "head.v"
 module REGISTER_FILE_TB(
 );
-    reg [DATA_WID - 1:0] data;
+    reg [DATA_WID - 1:0] valA, valB, valE, valM;
+    reg [ADDR_WID - 1:0] srcA, srcB, destE, destM;
+    reg [3:0]i;
+    reg [3:0]j;
+    reg [3:0]k = 0;
+    reg CLK = 0;
+    always begin
+        #5;
+        CLK = ~CLK;
+    end
+    REGESTER_FILE regester_file(
+        .CLK(CLK),
+        .valA(valA),
+        .valB(valB),
+        .srcA(srcA),
+        .srcB(srcB),
+        .valE(valE),
+        .valM(valM),
+        .destE(destE),
+        .destM(destM)
+    );
     initial begin
-        
+        for (i = 4'b0000; i <= 4'b0011; ++i) begin
+            valA = 10 + 5 * i;
+            srcA = i;
+            #5;
+        end
+        for (j = 4'b1111; j >= 4'b0100; --j) begin
+            valB = 10 + 5 * j;
+            srcB = j;
+            #5;
+        end
+    end
+
+    initial begin
+        #100;
+        for (k = 4'b0000; k <= 4'b1111; k++) begin
+            destE = k;
+            destM = 15 - k;
+            #1;
+        end
+    end
+endmodule
 
