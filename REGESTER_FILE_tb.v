3333934ab5acd7e5d04a08002352919466ee2206
YanB25 <442826556@qq.con>
2017-06-07 20:04:03 +0800

    update registfile tb

--- REGESTER_FILE_tb.v
+++ REGESTER_FILE_tb.v
@@ -3,61 +3,6 @@
 `include "head.v"
 module REGISTER_FILE_TB(
 );
-//    wire [`DATA_WID - 1:0] valA ;
-//    wire [`DATA_WID - 1:0] valB ;
-//    reg [`DATA_WID - 1:0] valE ;
-//    reg [`DATA_WID - 1:0] valM ;
-//    reg [`ADDR_WID - 1:0] srcA ;
-//    reg [`ADDR_WID - 1:0] srcB ;
-//    reg [`ADDR_WID - 1:0] destE ;
-//    reg [`ADDR_WID - 1:0] destM ;
-//    reg CLK;
-////    reg [3:0]i = 0;
-////    reg [3:0]j = 0;
-////    reg [3:0]k = 0;
-
-    
-    
-//    always begin
-//        #5;
-//        CLK = ~CLK;
-//    end
-    
-//    REGESTER_FILE regester_file(
-//        .CLK(CLK),
-//        .valA(valA),
-//        .valB(valB),
-//        .srcA(srcA),
-//        .srcB(srcB),
-//        .valE(valE),
-//        .valM(valM),
-//        .destE(destE),
-//        .destM(destM)
-//    );
-//    initial begin
-//        CLK = 0;
-//        destE = 0;
-//        valE = 0;
-//        destM = 0;
-//        valM = 0;
-//        #200; srcA = 2;
-//        #200; srcB = 5;
-//    end
-//    integer index = 0;
-//    initial begin
-//        for(index = 0; index < 16; index = index + 1) begin
-//            if (index == 0) begin
-//                 #10;
-//            end
-//            else begin
-//                destE = destE + 1;
-//                valE = valE + 1;
-//                #10;
-//             end
-//        end
-//    end
-
-
 
     wire [`DATA_WID - 1:0] valA = 0;
     wire [`DATA_WID - 1:0] valB = 0;
@@ -71,44 +16,107 @@ module REGISTER_FILE_TB(
     reg [3:0]j = 0;
     reg [3:0]k = 0;
     reg CLK = 0;
+    wire [3:0]OUTA;
+    wire [3:0]OUTB;
+    wire [3:0]OUTE;
+    wire [3:0]OUTM;
+    reg [`ADDR_WID - 1:0] rA;
+    reg [`ADDR_WID - 1:0] rB;
+    reg [3:0] icode;
+    
     always begin
         #5;
         CLK = ~CLK;
     end
+
+    SRC_A src_a(
+        .rA(rA),
+        .icode(icode),
+        .OUT(OUTA)
+    );
+    SCR_B src_b(
+        .rB(rB),
+        .icode(icode),
+        .OUT(OUTB)
+    );
+    DEST_E dest_e(
+        .rB(rB),
+        .icode(icode),
+        .Cnd(Cnd),
+        .OUT(OUTE)
+    );
+    DEST_M dest_m(
+        .rA(rA),
+        .icode(icode),
+        .OUT(OUTM)
+    );
     REGESTER_FILE regester_file(
         .CLK(CLK),
         .valA(valA),
         .valB(valB),
-        .srcA(srcA),
-        .srcB(srcB),
+        .srcA(OUTA),
+        .srcB(OUTB),
         .valE(valE),
         .valM(valM),
-        .destE(destE),
-        .destM(destM)
+        .destE(OUTE),
+        .destM(OUTM)
     );
-    integer number = 0;
     initial begin
-        #200;
-      for (i = 4'b0000; i <= 4'b0011; i = i + 1) begin
-            srcA = i;
-            #5;
-        end
-      for (j = 4'b1111; j >= 4'b0100; j = j - 1) begin
-            srcB = j;
-            #5;
-        end
+        //irmovq $(10) %rax
+        icode = `_IRMOV;
+        scrA = 'rdx_; //F
+        scrB = 'rsp_; //F
+        destE = 'rax_; //%rax
+        destM = 'rbx_; //F
+        valE = 10;
+        valM = 20; //not this
+
+        #5;
+        //rrmovq %rax %rcx
+        icode = `_RRMOVQ;
+        scrA = `rax_; //%rax
+        srcB = `rcx_; //F
+        destE = `rcx_; //%rcx
+        destM = `rsi_; //F
+        valE = 7; //
+        valM = 9; //
+
+        #5;
+        //call $(35)
+        icode = `_CALL;
+        scrA = `rcx_; //F
+        scrB = `NonReg_; //%rsp
+        destE = `rsi_; //%rsp
+        destM = `rax_; //F
+        valE = 35;
+        valM = 10; //
     end
 
-    initial begin
-//        #100;
-      for (k = 4'b0000; k <= 4'b1111; k = k + 1) begin
-            destE = k;
-            destM = 15 - k;
-            number = 10 * k;
-            #1;
-        end
-    end
-    assign valA = number;
-    assign valB = number;
+
+
+
+    //integer number = 0;
+    //initial begin
+    //    #200;
+    //  for (i = 4'b0000; i <= 4'b0011; i = i + 1) begin
+    //        srcA = i;
+    //        #5;
+    //  for (j = 4'b1111; j >= 4'b0100; j = j - 1) begin
+    //        srcB = j;
+    //        #5;
+    //    end
+    //end
+
+    //initial begin
+//  //      #100;
+    //  for (k = 4'b0000; k <= 4'b1111; k = k + 1) begin
+    //        destE = k;
+    //        destM = 15 - k;
+    //        number = 10 * k;
+    //        #1;
+    //    end
+    //end
+    //assign valA = number;
+    //assign valB = number;
 endmodule
 
