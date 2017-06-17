49c565d1cbb3646529f989f65a20cfbbdaa2df14
YanB25 <442826556@qq.con>
2017-06-17 16:26:53 +0800

    add every folder a *_MODULE file and finish top.v

---
+++ AlgorithmLogicUnit/ALU_MODULE.v
@@ -0,0 +1,63 @@
+`timescale 1ns/1ps
+`include "../header/head.v"
+
+module ALU_MODULE(
+    input [3:0] icode,
+    input [3:0] ifun,
+    input [`DATA_WID - 1:0]valA,
+    input [`DATA_WID - 1:0]valB,
+    input [`DATA_WID - 1:0]valC,
+    output Cnd,
+    output [`DATA_WID - 1:0]valE
+);
+    wire [`DATA_WID - 1:0]OUTA;
+    wire [`DATA_WID - 1:0]OUTB;
+    wire [1:0]OUTALUFun;
+    wire OUTSet_cc;
+    wire OUTCond;
+    wire [3:0]CC;
+
+    assign Cnd = OUTCond;
+
+    ALUA alua(
+        .valA(valA),
+        .valC(valC),
+        .icode(icode),
+        .OUT(OUTA)
+    );
+
+    ALUB alub(
+        .valB(valB),
+        .valC(valC),
+        .icode(icode),
+        .OUT(OUTB)
+    );
+
+    ALUFUN alufun(
+        .icode(icode),
+        .ifun(ifun),
+        .OUT(OUTALUFun)
+    );
+
+
+    SET_CC set_cc(
+        .icode(icode),
+        .OUT(OUTSet_cc)
+    );
+    
+    COND cond(
+        .ifun(ifun),
+        .CC(CC),
+        .OUT(OUTCond)
+    );
+
+    ALU alu(
+        .valE(valE),
+        .CC(CC),
+        .ALUfun(OUTALUFun),
+        .ALUA(OUTA),
+        .ALUB(OUTB),
+        .set_cond(OUTSet_cc)
+    );
+endmodule
+    
---
+++ top.v
@@ -0,0 +1,74 @@
+`timescale 1ns/1ps
+`include "header/head.v"
+module CPU(
+    input CLK
+);
+    wire [`DATA_WID - 1:0]PC;
+    wire [3:0] icode;
+    wire Cnd;
+    wire [`DATA_WID - 1:0] valC;
+    wire [`DATA_WID - 1:0] valM;
+    wire [`DATA_WID - 1:0] valP;
+    PC_MODULE pc_module(
+        .CLK(CLK),
+        .icode(icode),
+        .Cnd(Cnd),
+        .valC(valC),
+        .valM(valM),
+        .valP(valP),
+        .PC(PC)
+    )
+
+    wire [3:0] ifun;
+    wire [3:0] rA;
+    wire [3:0] rB;
+    INSTRU_MEM_MODULE instru_mem_module(
+        .PC(PC),
+        .icode(icode),
+        .ifun(ifun),
+        .rA(rA),
+        .rB(rB),
+        .valC(valC)
+    );
+
+
+    wire [`DATA_WID - 1:0] valE;
+    wire [`DATA_WID - 1:0] valA;
+    wire [`DATA_WID - 1:0] valB;
+    REGISTER_FILE_MODULE register_file_module(
+        .rA(rA),
+        .rB(rB),
+        .CLK(CLK),
+        .icode(icode),
+        .Cnd(Cnd),
+        .valE(valE),
+        .valM(valM),
+        .valA(valA),
+        .valB(valB)
+    );
+
+    ALU_MODULE alu_module(
+        .icode(icode),
+        .ifun(ifum),
+        .valA(valA),
+        .valB(valB),
+        .valC(valC),
+        .Cnd(Cnd),
+        .valE(valE)
+    );
+
+    wire instr_valid = 1; // TODO
+    wire imem_error = 0; //TODO
+    wire [1:0] stat; // TODO
+    
+    MEMORY_MODULE memory_module(
+        .valP(valP),
+        .valA(valA),
+        .valE(valE),
+        .icode(icode),
+        .instr_valid(instr_valid),
+        .imem_error(imem_error),
+        .stat(stat),
+        .valM(valM)
+    );
+endmodule
